9ebdbd6ec0fac124ca43288dad05146b31b2898a
FTC16750 <technototes@outlook.com>
2024-09-21 12:34:25 -0700

    added buttons and assigned them :)))

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/controls/OperatorController.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/controls/OperatorController.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.twenty403.controls;
 
+import com.technototes.library.command.Command;
 import com.technototes.library.control.CommandButton;
 import com.technototes.library.control.CommandGamepad;
 import org.firstinspires.ftc.twenty403.Robot;
@@ -10,18 +11,50 @@ public class OperatorController {
 
     public Robot robot;
     public CommandGamepad gamepad;
+    public CommandButton openRetainer;
+    public CommandButton closeRetainer;
+    public CommandButton eatRetainer;
+    public CommandButton biteJaw;
+    public CommandButton releaseJaw;
+    public CommandButton slurpIntake;
+    public CommandButton spitIntake;
+    public CommandButton stopIntake;
+
 
     public OperatorController(CommandGamepad g, Robot r) {
         this.robot = r;
         gamepad = g;
 
         AssignNamedControllerButton();
+
         BindControls();
     }
 
     private void AssignNamedControllerButton() {
+        openRetainer = gamepad.dpadUp;
+        closeRetainer = gamepad.dpadDown;
+        eatRetainer = gamepad.dpadRight;
+        slurpIntake = gamepad.leftBumper;
+        spitIntake = gamepad.rightBumper;
+        biteJaw = gamepad.ps_cross;
+        releaseJaw = gamepad.ps_triangle;
+
     }
 
     public void BindControls() {
+        if (Setup.Connected.KIDSSHAMPOOSUBSYSTEM){
+            bindKidShampooControls();
+        }
+    }
+    public void bindKidShampooControls() {
+        openRetainer.whenPressed(Command.create(robot.kidShampooSubsystem::openRetainer, robot.kidShampooSubsystem));
+        closeRetainer.whenPressed(Command.create(robot.kidShampooSubsystem::closeRetainer, robot.kidShampooSubsystem));
+        eatRetainer.whenPressed(Command.create(robot.kidShampooSubsystem::eatRetainer, robot.kidShampooSubsystem));
+        biteJaw.whenPressed(Command.create(robot.kidShampooSubsystem::biteJaw, robot.kidShampooSubsystem));
+        releaseJaw.whenPressed(Command.create(robot.kidShampooSubsystem::releaseJaw, robot.kidShampooSubsystem));
+        slurpIntake.whenPressed(Command.create(robot.kidShampooSubsystem::slurpIntake, robot.kidShampooSubsystem));
+        spitIntake.whenPressed(Command.create(robot.kidShampooSubsystem::spitIntake, robot.kidShampooSubsystem));
+        slurpIntake.whenReleased(Command.create(robot.kidShampooSubsystem::stopIntake, robot.kidShampooSubsystem));
+        spitIntake.whenReleased(Command.create(robot.kidShampooSubsystem::stopIntake, robot.kidShampooSubsystem));
     }
 }
