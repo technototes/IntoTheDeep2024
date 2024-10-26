db120392df4bbe759b093809e758caff8bc268be
Javier Alvarez <javier.alvarez.chiang@gmail.com>
2024-10-25 17:21:52 -0700

    fixed merge issues

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/commands/slides/SimpleSlidesCommands.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/commands/slides/SimpleSlidesCommands.java
@@ -6,23 +6,23 @@ import org.firstinspires.ftc.sixteen750.subsystems.VerticalSlidesSubsystem;
 public class SimpleSlidesCommands {
 
     public static Command HighBasket(VerticalSlidesSubsystem vs) {
-        Command.create(vs::SlideBucketHigh, vs);
+        return Command.create(vs::SlideBucketHigh, vs);
     }
 
     public static Command LowBasket(VerticalSlidesSubsystem vs) {
-        Command.create(vs::SlideBucketLow, vs);
+        return Command.create(vs::SlideBucketLow, vs);
     }
 
     public static Command HighChamber(VerticalSlidesSubsystem vs) {
-        Command.create(vs::SlideChamberHigh, vs);
+        return Command.create(vs::SlideChamberHigh, vs);
     }
 
     public static Command LowChamber(VerticalSlidesSubsystem vs) {
-        Command.create(vs::SlideChamberLow, vs);
+        return Command.create(vs::SlideChamberLow, vs);
     }
 
     public static Command LiftIntake(VerticalSlidesSubsystem vs) {
-        Command.create(vs::LiftHeightIntake, vs);
+        return Command.create(vs::LiftHeightIntake, vs);
     }
     //transfer
     //inc/dec
