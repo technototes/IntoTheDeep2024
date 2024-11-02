3ff45f9b480db09f33fd232d8c794b84242a368b
Braelyn <siennafox@gmail.com>
2024-11-02 15:15:06 -0700

    bucket and arm value testing, pretty accurate. may need to change transfer and add a movement position. made some sequentials, should add some without slide movement probably for lower bucket and eventually low chamber

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/controls/OperatorControllerVertical.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/controls/OperatorControllerVertical.java
@@ -14,6 +14,8 @@ public class OperatorControllerVertical {
     public Stick vertslidesLeftStick;
     public CommandGamepad gamepad;
     public CommandButton bucketTransfer;
+    public CommandButton bucketScore;
+    public CommandButton bucketIncrement;
     public CommandButton armTransfer;
     public CommandButton armScore;
     public CommandButton slidesLow;
@@ -30,10 +32,12 @@ public class OperatorControllerVertical {
 
     private void AssignNamedControllerButton() {
         bucketTransfer = gamepad.ps_circle;
+        bucketScore = gamepad.ps_triangle;
+        bucketIncrement =
         armTransfer = gamepad.ps_cross;
-        slidesLow = gamepad.ps_triangle;
-        slidesHigh = gamepad.dpadUp;
         armScore = gamepad.ps_square;
+        slidesLow = gamepad.dpadLeft;
+        slidesHigh = gamepad.dpadUp;
         slidesDown = gamepad.dpadDown;
         //wristDecrement = gamepad.dpadDown;
         vertslidesLeftStick = gamepad.leftStick;
@@ -46,11 +50,13 @@ public class OperatorControllerVertical {
         }
     }
 
+
     private void bindVerticalSlidesControls() {
         bucketTransfer.whenPressed(VerticalSlidesCommands.BucketTransfer(robot));
+        bucketScore.whenPressed(VerticalSlidesCommands.BucketEmpty(robot));
         armTransfer.whenPressed(VerticalSlidesCommands.ArmTransfer(robot));
         armScore.whenPressed(VerticalSlidesCommands.ArmScore(robot));
-        slidesHigh.whenPressed(VerticalSlidesCommands.LowBasket(robot));
+        slidesHigh.whenPressed(VerticalSlidesCommands.HighBasket(robot));
         slidesLow.whenPressed(VerticalSlidesCommands.LowBasket(robot));
         slidesDown.whenPressed(VerticalSlidesCommands.SlidesZero(robot));
         //wristDecrement.whenPressed(SlidesCommands.wristDecrement(robot));
