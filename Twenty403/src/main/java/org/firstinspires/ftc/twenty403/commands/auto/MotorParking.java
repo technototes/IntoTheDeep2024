8ad613d1c223ca4bc3b3212058c5766b2fa7d5c0
FTC16750 <technototes@outlook.com>
2024-11-02 14:33:47 -0700

    11/1/24 - Harshini everything but rotation has been tested on the bot attempted to create an auto file for observation auto(this might change soon) adjusted scaling constants for the wheels and scaled down the rotation so it isnt crazy

---
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/commands/auto/MotorParking.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.twenty403.commands.auto;
+
+import com.technototes.library.command.Command;
+import com.technototes.library.command.SequentialCommandGroup;
+import com.technototes.path.command.TrajectorySequenceCommand;
+
+import org.firstinspires.ftc.twenty403.AutoConstants;
+import org.firstinspires.ftc.twenty403.Robot;
+import org.firstinspires.ftc.twenty403.subsystems.DrivebaseSubsystem;
+
+public class MotorParking implements Command {
+    public DrivebaseSubsystem subsystem;
+    public double p;
+
+    public MotorParking(DrivebaseSubsystem s, double power) {
+        subsystem = s;
+        p = power;
+    }
+//    public static Command motorPower(Robot r) {
+//        return new SequentialCommandGroup(
+//        Command.create(r.drivebaseSubsystem::setMotorPark(1.0, 1.0, 1.0, 1.0)),
+//                r.drivebaseSubsystem.setMotorPowers(0,0,0,0)
+//        );
+//    }
+
+    public void execute() {
+        subsystem.setMotorPowers(-p, p, -p, p);
+    }
+
+
+    }
---
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/opmodes/JustPark.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.twenty403.opmodes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.technototes.library.command.CommandScheduler;
+import com.technototes.library.command.SequentialCommandGroup;
+import com.technototes.library.structure.CommandOpMode;
+import com.technototes.library.util.Alliance;
+
+import org.firstinspires.ftc.twenty403.AutoConstants;
+import org.firstinspires.ftc.twenty403.Hardware;
+import org.firstinspires.ftc.twenty403.Robot;
+import org.firstinspires.ftc.twenty403.commands.auto.ForwardBackwardCommand;
+import org.firstinspires.ftc.twenty403.controls.DriverController;
+import org.firstinspires.ftc.twenty403.controls.SafetyTestController;
+import org.firstinspires.ftc.twenty403.helpers.StartingPosition;
+
+@Autonomous(name = "JustPark")
+@SuppressWarnings("unused")
+public class JustPark extends CommandOpMode {
+
+    public Robot robot;
+    public DriverController controls;
+    public SafetyTestController safety;
+    public Hardware hardware;
+
+    @Override
+    public void uponInit() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        hardware = new Hardware(hardwareMap);
+        robot = new Robot(hardware, Alliance.RED, StartingPosition.Wing);
+        robot.drivebaseSubsystem.setPoseEstimate(AutoConstants.BACKWARD.toPose());
+        // safety = new SafetyTestController(driverGamepad, robot);
+        // robot.safetySubsystem.startMonitoring();
+        CommandScheduler.scheduleForState(
+            new SequentialCommandGroup(
+                new ForwardBackwardCommand(robot),
+                // EZCmd.Drive.RecordHeading(robot.drivebaseSubsystem),
+                CommandScheduler::terminateOpMode
+            ),
+            OpModeState.RUN
+        );
+
+//        CommandScheduler.scheduleForState(
+//            new SafetyStartCommand(robot.safetySubsystem),
+//            OpModeState.RUN
+//        );
+    }
+}
