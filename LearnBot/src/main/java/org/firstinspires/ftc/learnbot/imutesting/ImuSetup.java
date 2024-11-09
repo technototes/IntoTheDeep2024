82854dff6e62f791a3883a837bc9d6d3aa4f8663
Kevin Frei <kevinfrei@hotmail.com>
2024-11-09 11:52:03 -0800

    External IMU code samples

---
+++ LearnBot/src/main/java/org/firstinspires/ftc/learnbot/imutesting/ImuSetup.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.learnbot.imutesting;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class ImuSetup {
+
+    public static String INTERNAL_IMU = "imu";
+    public static String EXTERNAL_IMU = "adafruit-imu";
+}
