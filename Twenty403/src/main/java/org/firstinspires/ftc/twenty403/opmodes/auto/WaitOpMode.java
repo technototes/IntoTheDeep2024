c17ea4d4a32553e2e639984b557d3a77341b6276
Festivepotatoes <109926357+Festivepotatoes@users.noreply.github.com>
2024-11-26 17:39:31 -0800

    Changed-odometry-orientation-in-hardware

---
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/opmodes/auto/WaitOpMode.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.twenty403.opmodes.auto;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.technototes.library.command.CommandScheduler;
+import com.technototes.library.command.SequentialCommandGroup;
+import com.technototes.library.command.WaitCommand;
+import com.technototes.library.structure.CommandOpMode;
+import com.technototes.library.util.Alliance;
+
+import org.firstinspires.ftc.twenty403.AutoConstants;
+import org.firstinspires.ftc.twenty403.Hardware;
+import org.firstinspires.ftc.twenty403.Robot;
+import org.firstinspires.ftc.twenty403.commands.EZCmd;
+import org.firstinspires.ftc.twenty403.commands.auto.Paths;
+import org.firstinspires.ftc.twenty403.controls.DriverController;
+import org.firstinspires.ftc.twenty403.controls.SafetyTestController;
+import org.firstinspires.ftc.twenty403.helpers.StartingPosition;
+
+//TODO: figure out where the other splineTest is coming from (duplicate spline error)
+@Autonomous(name = "WaitOpMode")
+@SuppressWarnings("unused")
+public class WaitOpMode extends CommandOpMode {
+
+    public Robot robot;
+    public DriverController controls;
+    public SafetyTestController safety;
+    public Hardware hardware;
+
+    @Override
+    public void uponInit() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        hardware = new Hardware(hardwareMap);
+        robot = new Robot(hardware, Alliance.RED, StartingPosition.Observation);
+        robot.drivebaseSubsystem.setPoseEstimate(AutoConstants.REST.toPose());
+        //        safety = new SafetyTestController(driverGamepad, robot);
+        //robot.safetySubsystem.startMonitoring();
+        CommandScheduler.scheduleForState(
+                new SequentialCommandGroup(
+
+                        new WaitCommand(30),
+                        CommandScheduler::terminateOpMode
+                ),
+                OpModeState.RUN
+        );
+        //        CommandScheduler.scheduleForState(
+        //            new SafetyStartCommand(robot.safetySubsystem),
+        //            OpModeState.RUN
+        //        );
+    }
+}
