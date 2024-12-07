188552beeef250f2aa829b93e5eaf26043ed7b3b
Kevin Frei <kevinfrei@hotmail.com>
2024-12-07 08:12:21 -0800

    Updated formating, too

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/commands/HighSpecimenCommand.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/commands/HighSpecimenCommand.java
@@ -4,31 +4,28 @@ import com.technototes.library.command.Command;
 import com.technototes.library.command.ParallelCommandGroup;
 import com.technototes.library.command.SequentialCommandGroup;
 import com.technototes.library.command.WaitCommand;
-
 import org.firstinspires.ftc.twenty403.Robot;
 
 public class HighSpecimenCommand {
 
-
-        public static SequentialCommandGroup HighSpecimenPreArm(Robot r) {
-            return new SequentialCommandGroup(
-                    Command.create(r.kidShampooSubsystem::releaseJaw, r.kidShampooSubsystem),
-                    Command.create(r.kidShampooSubsystem::openRetainer, r.kidShampooSubsystem),
-                    Command.create(r.kidShampooSubsystem::straightWrist, r.kidShampooSubsystem)
-            );
-        }
-
-        public static SequentialCommandGroup HighSpecimen(Robot r) {
-            return new SequentialCommandGroup(
-                    Command.create(r.kidShampooSubsystem::stopIntake),
-                    Command.create(r.armSubsystem::setSlideToZero, r.armSubsystem),
-                    new WaitCommand(0.5),
-                    Command.create(r.armSubsystem::highSpecimen, r.armSubsystem),
-                    new WaitCommand(0.5),
-                    Command.create(r.armSubsystem::highSpecimenSlides, r.armSubsystem),
-                    new WaitCommand(0.5),
-                    HighSpecimenPreArm(r)
-            );
-        }
+    public static SequentialCommandGroup HighSpecimenPreArm(Robot r) {
+        return new SequentialCommandGroup(
+            Command.create(r.kidShampooSubsystem::releaseJaw, r.kidShampooSubsystem),
+            Command.create(r.kidShampooSubsystem::openRetainer, r.kidShampooSubsystem),
+            Command.create(r.kidShampooSubsystem::straightWrist, r.kidShampooSubsystem)
+        );
     }
 
+    public static SequentialCommandGroup HighSpecimen(Robot r) {
+        return new SequentialCommandGroup(
+            Command.create(r.kidShampooSubsystem::stopIntake),
+            Command.create(r.armSubsystem::setSlideToZero, r.armSubsystem),
+            new WaitCommand(0.5),
+            Command.create(r.armSubsystem::highSpecimen, r.armSubsystem),
+            new WaitCommand(0.5),
+            Command.create(r.armSubsystem::highSpecimenSlides, r.armSubsystem),
+            new WaitCommand(0.5),
+            HighSpecimenPreArm(r)
+        );
+    }
+}
