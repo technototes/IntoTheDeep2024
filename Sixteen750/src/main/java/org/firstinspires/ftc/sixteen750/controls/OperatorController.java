72d30b3231bfbb1886a15c846ff731a39ce849d8
Braelyn <siennafox@gmail.com>
2024-12-07 09:49:00 -0800

    button bindings, please merge

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/controls/OperatorController.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/controls/OperatorController.java
@@ -92,31 +92,11 @@ public class OperatorController {
         openClaw_closeClaw = gamepad.rightBumper;
         wristTransfer_wristPickup = gamepad.ps_square;
         bucketTransfer_bucketEmpty = gamepad.leftBumper;
-        //armTransfer_armEmpty = gamepad.dpadLeft;
-        //closeClaw = gamepad.rightBumper;
-        //wristTransfer = gamepad.ps_square;
-        //wristPickup = gamepad.ps_circle;
-        //wristZero = gamepad.ps_options;
-        //wristDecrement = gamepad.dpadRight;
-        //wristIncrement = gamepad.dpadLeft;
-
+        armTransfer_armEmpty = gamepad.dpadLeft;
         horiSlidesManual = gamepad.rightStick;
-        //horislidesExtend = gamepad.ps_cross;
-        //horislidesRetract = gamepad.ps_square;
 
-        //vertical commands
-        //bucketTransfer = gamepad.ps_circle;
-        //bucketScore = gamepad.ps_triangle;
 
-        //bucketIncrement = gamepad.dpadDown;
-        //bucketDecrement = gamepad.dpadDown;
-
-        //choice commands
-        /*shiftButton = gamepad.ps_share; - need to change button location and set up choice commands*/
-        //horizontalSlides_verticalSlides = gamepad.ps_triangle;
-        //slidesHigh = gamepad.dpadUp;
         slidesLowSequential = gamepad.dpadUp;
-        //slidesDown = gamepad.dpadDown;
         slidesZero = gamepad.ps_share;
         extend_retract = gamepad.ps_cross;
         down_high = gamepad.dpadDown;
@@ -124,41 +104,25 @@ public class OperatorController {
 
     private void bindSlidesControls() {
         /*shiftButton.whenPressed(this::toggleShift); // might use to have a manual and non manual mode*/
-        openClaw_closeClaw.whenPressed(HorizontalSlidesCommands.clawToggle(robot));
-        wristTransfer_wristPickup.whenPressed(HorizontalSlidesCommands.wristToggle(robot));
-        extend_retract.whenPressed(HorizontalSlidesCommands.horiSlideToggle(robot));
-        down_high.whenPressed(VerticalSlidesCommands.vertSlideToggle(robot));
+
         if (HORIZONTALSLIDESUBSYSTEM){
-        //closeClaw.whenPressed(HorizontalSlidesCommands.clawChomp(robot));
-//        wristPickup.whenPressed(HorizontalSlidesCommands.wristPickup(robot));
-//        wristTransfer.whenPressed(HorizontalSlidesCommands.wristTransfer(robot));
-        //wristDecrement.whenPressed(HorizontalSlidesCommands.wristIncrement(robot));//flipped but haven't flipped it in code
-        //wristIncrement.whenPressed(HorizontalSlidesCommands.wristDecrement(robot));
-        //horislidesExtend.whenPressed(HorizontalSlidesSequentials.intake(robot));
-        //horislidesRetract.whenPressed(HorizontalSlidesSequentials.transferring(robot));
-        //wristZero.whenPressed(HorizontalSlidesCommands.resetWristZero(robot));
-        CommandScheduler.scheduleJoystick(
+            extend_retract.whenPressed(HorizontalSlidesCommands.horiSlideToggle(robot));
+            openClaw_closeClaw.whenPressed(HorizontalSlidesCommands.clawToggle(robot));
+            wristTransfer_wristPickup.whenPressed(HorizontalSlidesCommands.wristToggle(robot));
+            CommandScheduler.scheduleJoystick(
                 new HorizontalAnalogCommand(robot.horizontalSlidesSubsystem, horiSlidesManual)
-        );
+            );
         }
         if (VERTICALSLIDESUBSYSTEM) {
+            down_high.whenPressed(VerticalSlidesCommands.vertSlideToggle(robot));
             bucketTransfer_bucketEmpty.whenPressed(VerticalSlidesCommands.bucketToggle(robot));
-            //bucketTransfer.whenPressed(VerticalSlidesCommands.BucketTransfer(robot));
-            //bucketEmpty.whenPressed(VerticalSlidesCommands.BucketEmpty(robot));
-            //bucketIncrement.whenPressed(VerticalSlidesCommands.BucketIncrement(robot));
-            //bucketDecrement.whenPressed(VerticalSlidesCommands.BucketDecrement(robot));
             armTransfer_armEmpty.whenPressed(VerticalSlidesCommands.armToggle(robot));
-            //armTransfer.whenPressed(VerticalSlidesCommands.ArmTransfer(robot));
-            //armEmpty.whenPressed(VerticalSlidesCommands.ArmEmpty(robot));
-//            armIncrement.whenPressed(VerticalSlidesCommands.ArmIncrement(robot));
-//            armDecrement.whenPressed(VerticalSlidesCommands.ArmDecrement(robot));
-            //slidesHigh.whenPressed(VerticalSlidesSequentials.HighBasket(robot));
+            //armIncrement.whenPressed(VerticalSlidesCommands.ArmIncrement(robot));
+            //armDecrement.whenPressed(VerticalSlidesCommands.ArmDecrement(robot));
             slidesLowSequential.whenPressed(VerticalSlidesSequentials.LowBasket(robot));
-            //slidesDown.whenPressed(VerticalSlidesSequentials.SlidesDown(robot));
             slidesZero.whenPressed(VerticalSlidesCommands.SlidesZero(robot));
-            //wristDecrement.whenPressed(SlidesCommands.wristDecrement(robot));
-            //        slidesUpTesting.whenPressed(VerticalSlidesCommands.SlidesUp(robot));
-            //        slidesDownTesting.whenPressed(VerticalSlidesCommands.SlidesDown(robot));
+            //slidesUpTesting.whenPressed(VerticalSlidesCommands.SlidesUp(robot));
+            //slidesDownTesting.whenPressed(VerticalSlidesCommands.SlidesDown(robot));
         }
     }
 }
