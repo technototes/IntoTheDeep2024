b16650f198e0b2a4e786cb43b985ce96ddb3cd8b
FTC16750 <technototes@outlook.com>
2024-12-12 19:40:30 -0800

    harshini 12/12/24
    made a isfinished command thingy for arm as well, it works!!
    
    auto is not finished at all :((
    
    intake compound command is not working for some reason, i need to figure this out
    
    still a huge work in progress :DD

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/commands/IntakePositionCommand.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/commands/IntakePositionCommand.java
@@ -1,18 +1,25 @@
 package org.firstinspires.ftc.twenty403.commands;
 
 import com.technototes.library.command.SequentialCommandGroup;
+import com.technototes.library.command.WaitCommand;
 import org.firstinspires.ftc.twenty403.Robot;
 
 public class IntakePositionCommand {
 
-    public static SequentialCommandGroup Intakepos(Robot r) {
+    public static SequentialCommandGroup IntakePos(Robot r) {
         return new SequentialCommandGroup(
             KidShampooCmds.cmds.OpenRetainer(r.kidShampooSubsystem),
             KidShampooCmds.cmds.StopIntake(r.kidShampooSubsystem),
             KidShampooCmds.cmds.DumpWrist(r.kidShampooSubsystem),
             ArmSubCmds.cmds.slideZero(r.armSubsystem),
             ArmSubCmds.cmds.intakePos(r.armSubsystem),
-            KidShampooCmds.cmds.CloseRetainer(r.kidShampooSubsystem)
+            new WaitCommand(1),
+            ArmSubCmds.cmds.intakePosSlides(r.armSubsystem),
+            new WaitCommand(1),
+            KidShampooCmds.cmds.CloseRetainer(r.kidShampooSubsystem),
+            KidShampooCmds.cmds.SlurpIntake(r.kidShampooSubsystem),
+            new WaitCommand(1),
+            KidShampooCmds.cmds.StopIntake(r.kidShampooSubsystem)
         );
     }
 }
