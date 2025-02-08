e757061471e52d2a0af5a987fe592c659e141cdd
Kevin Frei <kevinfrei@hotmail.com>
2025-02-07 17:16:26 -0800

    Launcher Code, ready for testing, not yet tested

---
+++ Hoops/src/main/java/org/firstinspires/ftc/hoops/commands/LaunchCommands.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.hoops.commands;
+
+import com.technototes.library.command.Command;
+import com.technototes.library.command.MethodCommand;
+import org.firstinspires.ftc.hoops.Robot;
+import org.firstinspires.ftc.hoops.subsystems.LauncherSubsystem;
+
+public class LaunchCommands {
+
+    public static Command launchCommand(Robot r) {
+        return Command.create(r.launcherSubsystem::Launch, 45.0);
+    }
+}
---
+++ Hoops/src/main/java/org/firstinspires/ftc/hoops/controllers/OperatorController.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.hoops.controllers;
+
+import com.technototes.library.command.CommandScheduler;
+import com.technototes.library.control.CommandAxis;
+import com.technototes.library.control.CommandButton;
+import com.technototes.library.control.CommandGamepad;
+import com.technototes.library.control.Stick;
+import org.firstinspires.ftc.hoops.Robot;
+import org.firstinspires.ftc.hoops.Setup;
+import org.firstinspires.ftc.hoops.commands.EZCmd;
+import org.firstinspires.ftc.hoops.commands.JoystickDriveCommand;
+import org.firstinspires.ftc.hoops.commands.LaunchCommands;
+
+public class OperatorController {
+
+    public Robot robot;
+    public CommandGamepad gamepad;
+
+    public CommandButton launch;
+
+    public OperatorController(CommandGamepad g, Robot r) {
+        this.robot = r;
+        gamepad = g;
+
+        AssignNamedControllerButton();
+        if (Setup.Connected.LAUNCHER) {
+            bindLaunchControls();
+        }
+    }
+
+    private void AssignNamedControllerButton() {
+        launch = gamepad.ps_square;
+    }
+
+    public void bindLaunchControls() {
+        launch.whenPressed(LaunchCommands.launchCommand(robot));
+    }
+}
